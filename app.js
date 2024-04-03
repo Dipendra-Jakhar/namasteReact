5e94cf2ac363ae49f350ddcd577cff852fe8b587
Dipendra-Jakhar <dipendra10oct@gmail.com>
2024-04-03 22:35:46 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+const parent = React.createElement("div", { id: "parent"}, [
+    React.createElement("div", { id: "child"}, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2"}, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+]);
+
+
+
+// const heading = React.createElement(
+//     "h1", 
+//     { id: "heading", abc: "xyz"}, 
+//     "Hello World from React!"
+//     );
+
+    console.log(parent); // object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
